b41a66371b3e1caac472cf773e07f9f0d756335d
LABORATÓRIO 803 <lab803@dominio.anhembimorumbi.edu.br>
2018-10-31 09:27:58 -0300

    modified:   OEC/src/cliente/cliente.java 	modified:   OEC/src/control/control.java 	modified:   OEC/src/oec/server.java

--- OEC/src/cliente/cliente.java
+++ OEC/src/cliente/cliente.java
@@ -1,21 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cliente;
 
-/**
- *
- * @author lab803
- */
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
 public class cliente {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        DatagramSocket cliente; 
+        DatagramPacket pacote; 
+        InetAddress ender;
+        Scanner texto;
+        @SuppressWarnings("UnusedAssignment")
+        byte[] dados = new byte[1024];
+        
+        try {
+            ender = InetAddress.getByName("127.0.0.1");
+            String msg = "";
+            while(!msg.equals("exit")){
+                texto = new Scanner(System.in);
+                msg = texto.nextLine();
+
+                dados = msg.getBytes();
+                pacote = new DatagramPacket(dados, dados.length, ender, 4448);
+                cliente = new DatagramSocket();
+                cliente.send(pacote);
+                
+                cliente.receive(pacote);
+                msg = new String(pacote.getData());
+                msg = msg.trim();
+                System.out.println("Recebido: " + msg);
+            }
+            
+            
+            
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+            
+        
     }
     
 }
